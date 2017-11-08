0f2cdd33cbfa62f9c9cec25f6cf67ecbfaf0b579
LAB18 <LAB18@DESKTOP-0RAG06Q>
2017-11-08 20:55:23 -0300

    Llamando alos elemento que voy a utilizar

---
+++ src/windows1/Java2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package windows1;
+import javax.swing.*; //Agregando import
+import java.awt.*;
+
+/**
+ *
+ * @author LAB18
+ */
+public class Java2 extends JFrame //Llamando a los elemento que voy a utilizar
+{
+    JPanel panel;
+    JLabel label;
+    JTextField text;
+    JButton boton;  
+}
