ffb9ccf40aa3dbdb65ae70d6bc841b2ba75ef4ee
LAB18 <LAB18@DESKTOP-0RAG06Q>
2017-11-08 21:30:56 -0300

    Terminando mi trabajo de gràfica

--- src/javaswing/JavaSwing.java
+++ src/javaswing/JavaSwing.java
@@ -17,7 +17,14 @@ public class JavaSwing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         // TODO code application logic here
+          Java2 ventana = new Java2(); //creo un obejeto tipo Windows
+        ventana.setVisible(true);
+        ventana.setBounds(0, 0,600, 800);
+        ventana.setTitle("Mi primer interfaz gràfica");// Aqui se coloca el nombre del titulo
+        //La siguente sentencia es para finelizar el proyecto al cerra la ventana
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
     
 }
